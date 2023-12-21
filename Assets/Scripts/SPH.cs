fa18b09d0275321910992280dfebb1da316d6a79
TeoEnKi <teoenki@gmail.com>
2023-12-21 09:07:23 +0800

    Revert "including the bitonic sort algorithm"
    This reverts commit 77ee5e9afc186050449872d8895db4778c5bae05.

--- Assets/Scripts/SPH.cs
+++ Assets/Scripts/SPH.cs
@@ -67,55 +67,13 @@ public class SPH : MonoBehaviour
     //store and manage particle data
     private ComputeBuffer particlesBuffer;
 
-    //for optimisation
-    private ComputeBuffer particleIndicesBuffer;
-    private ComputeBuffer particleCellIndicesBuffer;
-    private ComputeBuffer cellOffsetsBuffer;
-    private ComputeBuffer CalculateCellOffset;
-
     private void Awake()
     {
         boundary = FindAnyObjectByType<Boundary>();
 
         SpawnParticles();
 
-        //Setup Arguements for Instanced Particle Rendering
-        uint[] args =
-        {
-            //get index of mesh (ref to vertices needed to draw mesh)
-            particleMesh.GetIndexCount(0),
-            //num of particles to instanciate
-            (uint)totalParticles,
-            //to know where to begin drawing mesh
-            particleMesh.GetIndexStart(0),
-            particleMesh.GetBaseVertex(0),
-            0 , //offset 
-        };
-
-        //number of elements, byte size 
-        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
-        argsBuffer.SetData(args);
-
-        //Setup Particle Buffer for intancing particles
-        particlesBuffer = new ComputeBuffer(totalParticles, 40);
-        //list of particle objects in buffer list
-        particlesBuffer.SetData(particles);
         //set up the buffers for the kernel in the compute shader
-
-        //store ID of particle
-        particleIndicesBuffer = new ComputeBuffer(totalParticles, 4);
-        //store hashed cell index for a particle
-        particleCellIndicesBuffer = new ComputeBuffer(totalParticles, 4);
-        //stare the  starting index for the particle indices buffer to know where the list of partcles with that desired key are stored
-        cellOffsetsBuffer = new ComputeBuffer(totalParticles, 4);
-
-        uint[] particleIndices = new uint[totalParticles];
-
-        for (int i = 0; i< particleIndices.Length; i++)
-        {
-            particleIndicesBuffer.SetData(particleIndices);
-        }
-
         SetupComputeBuffers();
     }
 
@@ -153,10 +111,6 @@ public class SPH : MonoBehaviour
                 shader.SetFloat("sphereRadius", moveball.transform.localScale.x / 2);
             }
         }
-        else
-        {
-            Debug.Log("Late");
-        }
 
         boxSize = boundary.boxSize;
         boxSpawn = boundary.transform.position;
@@ -166,24 +120,20 @@ public class SPH : MonoBehaviour
         shader.SetVector("boxSize", boxSize);
         shader.SetVector("boxSpawn", boxSpawn);
 
-        //run the kernels //!!ENSURE TOTALPARTICALS IS DIVISIBLE BY 256 (because of the bitonic sort) (args are the num of thread group)
-        shader.Dispatch(hashParticlesKernel, totalParticles / 256, 1, 1);
+        //run the kernels //!!ENSURE TOTALPARTICALS IS DIVISIBLE BY 50
+        shader.Dispatch(integrateKernel, totalParticles / 50, 1, 1);
+        shader.Dispatch(densityKernel, totalParticles / 50, 1, 1);
+        shader.Dispatch(pressureKernel, totalParticles / 50, 1, 1);
+        shader.Dispatch(viscosityKernel, totalParticles / 50, 1, 1);
+        shader.Dispatch(externalKernal, totalParticles / 50, 1, 1);
 
-        SortParticles();
-
-        shader.Dispatch(calculateCellOffsetKernel, totalParticles / 256, 1, 1);
-
-        shader.Dispatch(integrateKernel, totalParticles / 256, 1, 1);
-        shader.Dispatch(densityKernel, totalParticles / 256, 1, 1);
-        shader.Dispatch(pressureKernel, totalParticles / 256, 1, 1);
-        shader.Dispatch(viscosityKernel, totalParticles / 256, 1, 1);
-        shader.Dispatch(externalKernal, totalParticles / 256, 1, 1);
 
         //render the particles (show it in game scene)
         particleMaterial.SetFloat(SizeProperty, particleRenderSize);
         particleMaterial.SetBuffer(ParticleBufferProperty, particlesBuffer);
 
         particlesBuffer.GetData(particles);
+
     }
 
 
@@ -227,42 +177,42 @@ public class SPH : MonoBehaviour
         particles = tempParticles.ToArray();
 
     }
-
-    private void SortParticles()
-    {
-        for(var dim = 2; dim <= totalParticles; dim <<= 1)
-        {
-            shader.SetInt("dim", dim);
-            for (var block = dim >> 1; block >0; block >>= 1)
-            {
-                shader.SetInt("block", block);
-                shader.Dispatch(biSortKernel, totalParticles / 256, 1, 1);
-            }
-        }
-    }
-
     private int integrateKernel;
     private int densityKernel;
     private int pressureKernel;
     private int viscosityKernel;
     private int externalKernal;
 
-    private int hashParticlesKernel;
-    private int biSortKernel;
-    private int calculateCellOffsetKernel;
-
     //link this to the SPHcomputeshader file and add the values to the variables that have their names match to the strings (keys)
     private void SetupComputeBuffers()
     {
-        //find and give kernel id for ease of use instead of writing the kernel name many times
+        //Setup Arguements for Instanced Particle Rendering
+        uint[] args =
+        {
+            //get index of mesh (ref to vertices needed to draw mesh)
+            particleMesh.GetIndexCount(0),
+            //num of particles to instanciate
+            (uint)totalParticles,
+            //to know where to begin drawing mesh
+            particleMesh.GetIndexStart(0),
+            particleMesh.GetBaseVertex(0),
+            0 , //offset 
+        };
+
+        //number of elements, byte size 
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        argsBuffer.SetData(args);
+
+        //Setup Particle Buffer for intancing particles
+        particlesBuffer = new ComputeBuffer(totalParticles, 40);
+        //list of particle objects in buffer list
+        particlesBuffer.SetData(particles);
+
         integrateKernel = shader.FindKernel("Integrate");
         densityKernel = shader.FindKernel("ComputeDensity");
         pressureKernel = shader.FindKernel("ComputePressureForce");
         viscosityKernel = shader.FindKernel("ComputeViscosityForce");
         externalKernal = shader.FindKernel("ExternalForces");
-        hashParticlesKernel = shader.FindKernel("HashParticles");
-        biSortKernel = shader.FindKernel("BitonicSort");
-        calculateCellOffsetKernel = shader.FindKernel("CalculateCellOffset");
 
         shader.SetFloat("renderRadius", particleRenderSize);
 
@@ -290,31 +240,6 @@ public class SPH : MonoBehaviour
         shader.SetBuffer(pressureKernel, "particles", particlesBuffer);
         shader.SetBuffer(viscosityKernel, "particles", particlesBuffer);
         shader.SetBuffer(externalKernal, "particles", particlesBuffer);
-        shader.SetBuffer(hashParticlesKernel, "particles", particlesBuffer);
-
-        //these kernels need these buffers as they are comparing with neighbouring particles for computing
-        shader.SetBuffer(densityKernel, "particleIndices", particleIndicesBuffer);
-        shader.SetBuffer(densityKernel, "particleCellIndices", particleCellIndicesBuffer);
-        shader.SetBuffer(densityKernel, "cellOffsets", cellOffsetsBuffer);
-
-        shader.SetBuffer(pressureKernel, "particleIndices", particleIndicesBuffer);
-        shader.SetBuffer(pressureKernel, "particleCellIndices", particleCellIndicesBuffer);
-        shader.SetBuffer(pressureKernel, "cellOffsets", cellOffsetsBuffer);
-
-        shader.SetBuffer(viscosityKernel, "particleIndices", particleIndicesBuffer);
-        shader.SetBuffer(viscosityKernel, "particleCellIndices", particleCellIndicesBuffer);
-        shader.SetBuffer(viscosityKernel, "cellOffsets", cellOffsetsBuffer);
-
-        shader.SetBuffer(hashParticlesKernel, "particleIndices", particleIndicesBuffer);
-        shader.SetBuffer(hashParticlesKernel, "particleCellIndices", particleCellIndicesBuffer);
-        shader.SetBuffer(hashParticlesKernel, "cellOffsets", cellOffsetsBuffer);
-
-        shader.SetBuffer(biSortKernel, "particleIndices", particleIndicesBuffer);
-        shader.SetBuffer(biSortKernel, "particleCellIndices", particleCellIndicesBuffer);
-
-        shader.SetBuffer(calculateCellOffsetKernel, "particleIndices", particleIndicesBuffer);
-        shader.SetBuffer(calculateCellOffsetKernel, "particleCellIndices", particleCellIndicesBuffer);
-        shader.SetBuffer(calculateCellOffsetKernel, "cellOffsets", cellOffsetsBuffer);
 
     }
     void ReleaseBuffer()
